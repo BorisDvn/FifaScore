2d2ccc5ed2c5de6aa670af5229666653ed705d0d
BorisDvn <dovonon@th-brandenburg.de>
2021-03-14 16:07:40 +0100

    connectToFootApi:ok!

--- src/main/java/com/fifa/score/controllers/ClubController.java
+++ src/main/java/com/fifa/score/controllers/ClubController.java
@@ -3,7 +3,7 @@ package com.fifa.score.controllers;
 import com.fifa.score.models.Club;
 import com.fifa.score.models.League;
 import com.fifa.score.services.ClubService;
-import com.fifa.score.services.ResquestService;
+import com.fifa.score.services.RequestService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,20 +20,24 @@ public class ClubController {
 
     private final ClubService clubService;
 
-    private final ResquestService resquestService;
+    private final RequestService requestService;
 
 
-    public ClubController(ClubService clubService, ResquestService resquestService) {
+    public ClubController(ClubService clubService, RequestService requestService) {
         this.clubService = clubService;
-        this.resquestService = resquestService;
+        this.requestService = requestService;
     }
 
     @GetMapping("initialisation")
     public ResponseEntity<String> initialisationClub() {
 
         try {
-            ResponseEntity<Map> response = resquestService.connectToFootApi("https://api.football-data.org/v2/competitions/2021/teams?season=2020");
-            assert response != null;
+            ResponseEntity<Map> response = requestService.connectToFootApi("https://api.football-data.org/v2/competitions/2021/teams?season=2020");
+
+            //Objects.requireNonNull(response);
+
+            assert response != null :"tp" ;
+            //System.out.println("test1");
 
             @SuppressWarnings("unchecked")
             List<Object> clubApi = (List<Object>) Objects.requireNonNull(response.getBody()).get("teams");
@@ -41,7 +45,7 @@ public class ClubController {
             List<Club> clubs = new ArrayList<Club>();
             League l = new League();
             l.setId_league(2021);
-
+            //System.out.println(response);
             for (int i = 0; i < clubApi.size(); i++) {
                 Club club = new Club();
 
---
+++ src/main/java/com/fifa/score/services/RequestService.java
@@ -0,0 +1,44 @@
+package com.fifa.score.services;
+
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Service
+public class RequestService {
+    public ResponseEntity<Map> connectToFootApi(String url) {
+        // create an instance of RestTemplate
+        RestTemplate restTemplate = new RestTemplate();
+
+        // create headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Auth-Token", "1856b1a8efff4d118603a3a889e93e66");
+        // or with headers.set();
+
+        // build the request
+        HttpEntity<?> request = new HttpEntity<>(headers);
+
+        // make an HTTP GET request with headers
+        try {
+            ResponseEntity<Map> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    request,
+                    Map.class
+            );
+
+            if (response.getStatusCode() == HttpStatus.OK && response.hasBody()) {
+                //System.out.println("Request Successful.");
+                return response;
+            } else {
+                System.out.println("Request Failed");
+                return null;
+            }
+        } catch (Exception e) {
+            System.out.println("RequestService"+e.toString());
+            return null;
+        }
+    }
+}
